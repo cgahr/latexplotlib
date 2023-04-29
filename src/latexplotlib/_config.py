7be35142b3968a540cdc28208473f28bc160d3c7
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-04-29 22:21:04 +0200

    Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    This reverts commit 71bf924157aeff90ea3765703119201d5765366f.

--- src/latexplotlib/_config.py
+++ src/latexplotlib/_config.py
@@ -19,11 +19,6 @@ ConfigData = Union[int, bool]
 
 
 class Config:
-
-    def _open(self, path: Path) -> Dict[str, ConfigData]:
-        with path.open(encoding="utf-8") as fh:
-            config: Dict[str, ConfigData] = json.load(fh)
-        return config
     def __init__(self, path: Path) -> None:
         self.path = path
 
@@ -31,6 +26,11 @@ class Config:
             self.reset()
 
         self._config = self._open(path)
+
+    def _open(self, path: Path) -> Dict[str, ConfigData]:
+        with path.open(encoding="utf-8") as fh:
+            config: Dict[str, ConfigData] = json.load(fh)
+        return config
 
     def _write(self, cfg: Mapping[str, ConfigData]) -> None:
         with self.path.open("w", encoding="utf-8") as fh:
