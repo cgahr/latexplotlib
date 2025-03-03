7f172e81008c4c2fcba253954f8bb3c1985efae9
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2025-03-03 21:48:10 +0100

    use uv2nix to create python package

--- flake.nix
+++ flake.nix
@@ -5,57 +5,124 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     pyproject-nix = {
-      url = "github:nix-community/pyproject.nix";
+      url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      uv2nix,
       pyproject-nix,
       flake-utils,
+      pyproject-build-systems,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        inherit (nixpkgs) lib;
 
-        project = pyproject-nix.lib.project.loadPyproject { projectRoot = ./.; };
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        latexplotlib-overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+          # sourcePreference = "sdist";
+        };
+
+        pyprojectOverrides = _final: _prev: { };
+
         python = pkgs.python312;
+
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                latexplotlib-overlay
+                pyprojectOverrides
+              ]
+            );
+
       in
       {
+        packages.default = pythonSet.mkVirtualEnv "latexplotlib-env" workspace.deps.default;
+
         devShells.default =
           let
-            attrs = project.renderers.withPackages {
-              inherit python;
-              extras = [ "tests" ];
-              extraPackages =
-                python-pkgs: with python-pkgs; [
-                  ipykernel
-                  python-lsp-server
-                ];
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              root = "$REPO_ROOT";
             };
-            pythonEnv = python.withPackages attrs;
+
+            editablePythonSet = pythonSet.overrideScope (
+              lib.composeManyExtensions [
+                editableOverlay
+
+                (final: prev: {
+                  latexplotlib = prev.latexplotlib.overrideAttrs (old: {
+                    src = lib.fileset.toSource {
+                      root = old.src;
+                      fileset = lib.fileset.unions [
+                        (old.src + "/pyproject.toml")
+                        (old.src + "/README.md")
+                        (old.src + "/src/latexplotlib/")
+                      ];
+                    };
+
+                    nativeBuildInputs =
+                      old.nativeBuildInputs
+                      ++ final.resolveBuildSystem {
+                        editables = [ ];
+                      };
+                  });
+
+                })
+              ]
+            );
+            virtualenv = editablePythonSet.mkVirtualEnv "latexplotlib-dev-env" workspace.deps.all;
+
           in
           pkgs.mkShell {
-            packages =
-              [ pythonEnv ]
-              ++ (with pkgs; [
-                ruff
-                pre-commit
-                stdenv.cc.cc.libgcc # for pre-commit ruff auto patchelf
-                texliveMinimal
-              ]);
+            packages = [
+              virtualenv
+              pkgs.uv
+              pkgs.pre-commit
+              pkgs.texliveMinimal
+              pkgs.ruff
+            ];
+
             nativeBuildInputs = [ pkgs.autoPatchelfHook ];
             shellHook = ''
               patch=$(autoPatchelf ~/.cache/pre-commit/)
 
-              export PYTHONPATH="$(pwd)/src"
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
+
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = "${virtualenv}/bin/python";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
           };
       }
     );
